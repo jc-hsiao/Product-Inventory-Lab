9a65f37146fdc8149e7398e021054c3312c48914
Leila Hsiao <lsiao@zipcodes-MacBook-Pro-6.local>
2020-02-13 13:46:31 -0500

    add printer properties

--- src/main/java/models/Printer.java
+++ src/main/java/models/Printer.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Printer {
+    private int id;
+    private int size;
+    private String name;
+    private String brand;
+    private String inkType;
+    private boolean canConnectWifi;
+    private boolean hasColorOutput;
+    private boolean isForBusiness;
+
 }
